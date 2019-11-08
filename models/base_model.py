aedf06d8eddc4b86626bc9052097e3ae40fc1cab
rolandoquiroz <rolandoquiroz@gmail.com>
2019-11-08 22:21:32 +0000

    First base_model.py version

--- models/base_model.py
+++ models/base_model.py
@@ -18,19 +18,30 @@ class BaseModel:
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """
         String representation of class BaseModel instance
         """
-        return ('[{:s}] ({}) {}'.format(cls.__name__, self.id, self.__dict__))
+        return ('[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         """
-
+        Updates the public instance attribute updated_at
+        with the current datetime
         """
         self.updated_at = datetime.now()
-        pass
 
     def to_dict(self):
-        pass
+        """
+        Returns a dictionary containing all keys/values
+        of __dict__ of the instance.
+        """
+        my_dict = {}
+        my_dict = self.__dict__.copy()
+        my_dict.update(__class__ = self.__class__.__name__)
+        my_dict['created_at'] = my_dict['created_at'].isoformat()
+        my_dict['updated_at'] = my_dict['updated_at'].isoformat()
+
+        return my_dict
