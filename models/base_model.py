eba2944b9cf581311e57a4f42fc196bbf9fd6787
rolandoquiroz <rolandoquiroz@gmail.com>
2019-11-09 19:02:38 +0000

    base_model.py updated with Task 3

--- models/base_model.py
+++ models/base_model.py
@@ -16,9 +16,16 @@ class BaseModel:
         """
         Initialize instance attributes
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
