078cc01ee9722d61b4f2e16c6e10dda7bc2209a2
amartinezre05 <almartinezre5@gmail.com>
2019-11-12 04:40:53 +0000

    Add

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+models/__init__.py updated to create a unique
+FileStorage instance for hbnb application
+"""
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
