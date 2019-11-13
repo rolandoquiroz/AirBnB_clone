636709f17e2e2e4b4a7315bffd500c256f327c17
amartinezre05 <almartinezre5@gmail.com>
2019-11-13 03:18:50 +0000

    [add] Class review and public attributes

---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module class Review"""
+
+from models.base_model import BaseModel
+
+
+def Review(BaseModel):
+    """Class Review"""
+    place_id = ""
+    user_id = ""
+    text = ""
